40f221237f9edf9fca2e9fc94760584af5bdf0a6
Vlad Prokopev <prokopev.vladislav@artpix3d.com>
2021-07-03 19:48:33 -0500

    added laser machine pc's mac addresses

---
+++ ArtPix Dashboard/Models/EntityLogs/EntityLogsModel.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ArtPix_Dashboard.Models.EntityLogs
+{
+    public class Data
+    {
+        [JsonProperty("sku")]
+        public string Sku { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("points")]
+        public object Points { get; set; }
+
+        [JsonProperty("machine")]
+        public string Machine { get; set; }
+
+        [JsonProperty("machines")]
+        public List<string> Machines { get; set; }
+
+        [JsonProperty("file_name")]
+        public string FileName { get; set; }
+
+        [JsonProperty("order_ids")]
+        public List<int> OrderIds { get; set; }
+
+        [JsonProperty("laser_time")]
+        public string LaserTime { get; set; }
+
+        [JsonProperty("order_names")]
+        public List<string> OrderNames { get; set; }
+
+        [JsonProperty("product_ids")]
+        public List<int> ProductIds { get; set; }
+
+        [JsonProperty("count_in_product")]
+        public int CountInProduct { get; set; }
+
+        [JsonProperty("number_in_product")]
+        public int NumberInProduct { get; set; }
+
+        [JsonProperty("machine_assign_item_ids")]
+        public List<int> MachineAssignItemIds { get; set; }
+    }
+
+    public class Datum
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("entity_id")]
+        public int EntityId { get; set; }
+
+        [JsonProperty("entity_type")]
+        public string EntityType { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("event_date")]
+        public string EventDate { get; set; }
+
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public string UpdatedAt { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonProperty("first")]
+        public string First { get; set; }
+
+        [JsonProperty("last")]
+        public string Last { get; set; }
+
+        [JsonProperty("prev")]
+        public object Prev { get; set; }
+
+        [JsonProperty("next")]
+        public string Next { get; set; }
+    }
+
+    public class Meta
+    {
+        [JsonProperty("current_page")]
+        public int CurrentPage { get; set; }
+
+        [JsonProperty("from")]
+        public int From { get; set; }
+
+        [JsonProperty("last_page")]
+        public int LastPage { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("per_page")]
+        public string PerPage { get; set; }
+
+        [JsonProperty("to")]
+        public int To { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
+
+    public class EntityLogsModel
+    {
+        [JsonProperty("datum")]
+        public List<Datum> Datum { get; set; }
+
+        [JsonProperty("links")]
+        public Links Links { get; set; }
+
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+    }
+
+
+
+
+}
